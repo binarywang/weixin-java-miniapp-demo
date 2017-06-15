31f201d482be7a9c9fd59e5dbc0378fc6e9f7165
Binary Wang <binarywang@gmail.com>
2017-06-16 00:06:21 +0800

    初始版本

---
+++ README.md
@@ -0,0 +1,11 @@
+### 本项目为weixin-java-tools的Demo演示程序，基于Spring Boot构建，实现微信小程序开发功能。更多信息请查阅：https://github.com/Wechat-Group/weixin-java-tools
+
+[![Build Status](https://travis-ci.org/Wechat-Group/weixin-java-miniapp-demo.svg?branch=master)](https://travis-ci.org/Wechat-Group/weixin-java-miniapp-demo)
+-----------------------
+
+## 使用步骤：
+1. 配置：复制`/src/main/resources/application.yml.template` 生成application.yml文件，根据自己需要填写相关配置（需要注意的是：yml文件内的属性冒号后面的文字之前需要加空格，可参考已有配置，否则属性会设置不成功）；	
+1. 运行Java程序：`com.github.binarywang.demo.wechat.WxMaDemoApplication`；
+1. 打开shell或cmd，进入ngrok目录，运行 `ngrok -config ngrok.cfg -subdomain my-domain 8080` 如果运行失败，请更换my-domain为其它字符串，直至连接成功；
+1. 配置微信小程序中的接口地址：http://my-domain.tunnel.qydev.com/wechat/portal （注意my-domain要跟上面的一致，需要符合微信官方的要求）；
+	
---
+++ src/main/java/com/github/binarywang/demo/wechat/controller/WxMaUserController.java
@@ -0,0 +1,71 @@
+package com.github.binarywang.demo.wechat.controller;
+
+import cn.binarywang.wx.miniapp.api.WxMaService;
+import cn.binarywang.wx.miniapp.bean.WxMaJscode2SessionResult;
+import cn.binarywang.wx.miniapp.bean.WxMaUserInfo;
+import com.github.binarywang.demo.wechat.utils.JsonUtils;
+import me.chanjar.weixin.common.exception.WxErrorException;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 微信小程序用户接口
+ *
+ * @author <a href="https://github.com/binarywang">Binary Wang</a>
+ */
+@RestController
+@RequestMapping("/wechat/user")
+public class WxMaUserController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private WxMaService wxService;
+
+    /**
+     * 登陆接口
+     */
+    @GetMapping("login")
+    public String login(String code) {
+        if (StringUtils.isBlank(code)) {
+            return "empty jscode";
+        }
+
+        try {
+            WxMaJscode2SessionResult session = this.wxService.getUserService().getSessionInfo(code);
+            this.logger.info(session.getSessionKey());
+            this.logger.info(session.getOpenid());
+            this.logger.info(session.getExpiresin().toString());
+            //TODO 可以增加自己的逻辑，关联业务相关数据
+            return JsonUtils.toJson(session);
+        } catch (WxErrorException e) {
+            this.logger.error(e.getMessage(), e);
+            return e.toString();
+        }
+    }
+
+    /**
+     * <pre>
+     * 获取用户信息接口
+     * </pre>
+     */
+    @GetMapping("info")
+    public String info(String sessionKey, String signature, String rawData, String encryptedData, String iv) {
+        // 用户信息校验
+        if (!this.wxService.getUserService().checkUserInfo(sessionKey, rawData, signature)) {
+            return "user check failed";
+        }
+
+        // 解密用户信息
+        WxMaUserInfo userInfo = this.wxService.getUserService().getUserInfo(sessionKey, encryptedData, iv);
+
+        return JsonUtils.toJson(userInfo);
+    }
+
+}
